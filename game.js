fa93ecea453491dc38f4e0692e761f4659db814a
Gauravraj360 <70471493+Gauravraj360@users.noreply.github.com>
2024-02-12 15:52:07 +0530

    Update game.js
    Added touchstart property for mobile users.

--- game.js
+++ game.js
@@ -1,94 +1,99 @@
-// alert("javascript is working");
-var buttonColours = ["red", "blue", "green", "yellow"];
-var userClickedPattern = [];
-var gamePattern = [];
 
-var started = false;
-var level = 0;
+var userClickedPattern =[];
 
-$(document).keypress(function () {
+$('.btn').on("click touchstart",function(event){
+    event.preventDefault();
+    var id=$(this).attr('id');
+    handleClick(id);
+});
+
+var level=0;
+var started=false;
+$(document).on("keydown touchstart", function(){
     if (!started) {
         $("#level-title").text("level " + level);
         nextSequence();
         started = true;
     }
-
 });
 
-$(".btn").click(function () {
-    var userChosenColour = $(this).attr("id");
+var handleClick = function(id){
+    const userChosenColour= ""+id+"";
     userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
+    animatePress(id);
+    playSound(id);
+    checkAnswer(userClickedPattern.length-1);
+  
+}
 
-    // Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
-    checkAnswer(userClickedPattern.length - 1);
-});
+var buttonColors=["green","red","yellow","blue"];
+var gamePattern = [];
 
-
-function nextSequence() {
-    // Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
-    userClickedPattern = [];
-
+var nextSequence = function(){
+    userClickedPattern=[];
+    let randomNumber=Math.random();
+    randomNumber=Math.round(randomNumber*3);
+    var randomChosenColor = buttonColors[randomNumber];
+    
+    gamePattern.push(randomChosenColor);
+    document.querySelector("#"+randomChosenColor+"");
+    animation(randomChosenColor);
+    playSound(randomChosenColor);
     level++;
-    $("#level-title").text("level " + level);
-
-    var randumNumer = Math.floor(4 * Math.random());
-
-    var randomChosenColour = buttonColours[randumNumer];
-
-    gamePattern.push(randomChosenColour);
-
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeIn(100);
-
-    playSound(randomChosenColour);
-
-    // return randomChosenColour;
+    $("#level-title").html("Level "+level+"");
 }
 
 
-function checkAnswer(currentLevel) {
-    if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+var animatePress= function(currentColor){
+    $('.'+currentColor+'').addClass("pressed");
+    setTimeout(function(){
+        $('.'+currentColor+'').removeClass("pressed");
+    },100)
+}
+
+var playSound=function(name){
+    var sound = new Audio("sounds/"+name+".mp3");
+    sound.play();
+}
+
+var animation= function(button){
+    
+    $("#"+button).animate({
+        opacity:'0.1'
+    },"fast").animate({
+        opacity:'1'
+    },"fast");
+
+}
+ 
+var checkAnswer =function(currentLevel){
+   
+    var lastAnswer=userClickedPattern[currentLevel]; 
+   
+    if(lastAnswer===gamePattern[currentLevel]) {
         console.log("success");
-        if (gamePattern.length == userClickedPattern.length) {
-            setTimeout(function () {
-                nextSequence();
-            }, 1000);
-        }
-    } else {
-        console.log("wrong");
+    }
+    else{
         playSound("wrong");
         $("body").addClass("game-over");
         setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
         $("#level-title").text("Game Over, Press Any Key to Restart");
-        // Call startOver() if the user gets the sequence wrong.
         startOver();
-        // $(document).keypress(function () {
-        //     startOver();
-        // });
-
     }
-}
 
-function playSound(name) {
-
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-}
-
-function animatePress(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-
-    setTimeout(function () {
-        $("#" + currentColour).removeClass("pressed");
-    }, 100);
+    if(gamePattern.length===currentLevel+1){
+    setTimeout(function(){
+        nextSequence();
+    },1000);
+    
+    }  
+   
 }
 
 function startOver() {
     level = 0;
     gamePattern = [];
     started = false; 
-
 }
